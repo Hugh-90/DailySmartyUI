8e85617aaa128b3a0b23b162730e374b2530014a
hugh <hugh90@hotmail.es>
2021-07-24 19:35:46 +0200

    update

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -3,18 +3,18 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-      <div className="recent-posts">
-        <div className="recent-posts__wrapper">
-          <div className="recent-posts__heading">Recent Posts</div>
-          <ul className="recent-posts__posts">
-            <li>recent post 0</li>
-            <li>recent post 1</li>
-            <li>recent post 2</li>
-          </ul>
+        <div className="recent-posts">
+          <div className="recent-posts__wrapper">
+            <div className="recent-posts__heading">Recent Posts</div>
+            <ul className="recent-posts__posts">
+              <li>Recent Post 0</li>
+              <li>Recent Post 1</li>
+              <li>Recent Post 2</li>
+            </ul>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
-}
 
 export default SearchBar;
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -4,27 +4,27 @@ import  {Field, reduxForm } from 'redux-form';
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
-        console.log("trying to handle submit for query", query);
+        console.log('trying to handle subimit for query', query);
+      }
+    
+      renderInput(field) {
+        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+      }
+    
+      render() {
+    
+        const { handleSubmit } = this.props;
+    
+        return (
+          <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+            <Field name="query" component={this.renderInput}/>
+          </form>
+        )
+      }
     }
-
-    renderInput(field) {
-        return <input type="text" placeholder="Search DailySmarty" {...field.input}/>
-    }
-
-  render() {
-
-    const {handleSubmit} = this.props;
-
-    return (
-      <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-          <Field name="query" component={this.renderInput}/>
-      </form>
-    )
-  }
-}
 
 SearchBar = reduxForm({
     form: 'searchBar'
-  })(searchBar);
+  })(SearchBar);
 
 export default SearchBar;
