7f2254d68dfb124345c006741e49bfdfc9f5773c
hugh <hugh90@hotmail.es>
2021-07-25 18:53:31 +0200

    added exact route for home route and implemented withRouter HOC into the searchbar to navigate the user to results route

--- src/components/results.js
+++ src/components/results.js
@@ -1,11 +1,12 @@
-import React , {Component} from "react";
-import Logo from "./logo";
+import React, { Component } from 'react';
+import Logo from './logo';
 
 class Results extends Component {
     render() {
         return (
             <div>
-                <Logo />
+                <h1>Results</h1>
+                <Logo/>
             </div>
         )
     }
