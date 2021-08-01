ba8cdf1ea7cc4a4172e0db06130a32f0a8a735b0
hugh <hugh90@hotmail.es>
2021-08-01 16:56:41 +0200

    styled results component

--- src/components/results.js
+++ src/components/results.js
@@ -15,7 +15,7 @@ class Results extends Component {
 
     render() {
         return (
-            <div>
+            <div className="results">
                 <Logo size={55}/>
                 <SearchBar page="results" onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
                 <ResultsPosts/>
